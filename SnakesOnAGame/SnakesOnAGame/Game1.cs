2fa82fe64c15e2eb4565669ec79ff90676409cc7
rbnsfr <rbnsfr@outlook.com>
2014-10-23 23:13:06 -0400

    XNA - Going further

--- SnakesOnAGame/SnakesOnAGame/Game1.cs
+++ SnakesOnAGame/SnakesOnAGame/Game1.cs
@@ -21,13 +21,23 @@ namespace SnakesOnAGame
         List<Vector2> snake = new List<Vector2>();
         Vector2 food;
         Random rand = new Random();
-        Texture2D snakeTexture, overTexture, pelletTexture, background, gtexture, stexture;
+        Texture2D snakeTexture, overTexture, pelletTexture, pelletSM, background, gtexture, stexture, backgroundSM, spaceKey, spaceDesc;
         Vector2 direction = new Vector2(0, 1);
         bool snoopmode = false; // my class wants this to be recurring
         bool gameover = false;
+        bool showdirections = true;
+        bool snoopmodeFinished;
         Color col, ballcol;
         string title = "Snakes on a Game";
-        float speed;
+        float snoopTime;
+        float gameOverTime = 0;
+        bool gameOverSoundFinished = false;
+        Song song;
+        SoundEffect snoop, snoopend, lose;
+        SoundEffectInstance snoopi, snoopei, losei;
+
+        int millisecondsPerFrame;
+        int timeSinceLastUpdate = 0; // Accumulate the elapsed time
 
         public Game1()
         {
@@ -48,15 +58,15 @@ namespace SnakesOnAGame
         {
             // TODO: Add your initialization logic here
             snake.Add(new Vector2(4, 5));
-            snake.Add(new Vector2(4, 6));
-            snake.Add(new Vector2(4, 7));
-            snake.Add(new Vector2(4, 8));
-            snake.Add(new Vector2(4, 9));
-            snake.Add(new Vector2(4, 10));
+            snake.Add(new Vector2(4, 20));
+            snake.Add(new Vector2(4, 35));
+            snake.Add(new Vector2(4, 50));
+            snake.Add(new Vector2(4, 65));
+            snake.Add(new Vector2(4, 80));
 
             Rectangle bounds = this.Window.ClientBounds;
-            int irandx = rand.Next(bounds.Left, bounds.Right);
-            int irandy = rand.Next(bounds.Top, bounds.Bottom);
+            int irandx = rand.Next(16, bounds.Width - 16);
+            int irandy = rand.Next(16, bounds.Height - 16);
 
             food = new Vector2(irandx, irandy);
 
@@ -75,11 +85,54 @@ namespace SnakesOnAGame
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
             background = Content.Load<Texture2D>(@"Atari");
-            snakeTexture = Content.Load<Texture2D>(@"SQUARE");
+            backgroundSM = Content.Load<Texture2D>(@"AtariSnoopmode");
+            snakeTexture = Content.Load<Texture2D>(@"Square");
             overTexture = Content.Load<Texture2D>(@"GameOver");
-            pelletTexture = Content.Load<Texture2D>(@"SQUARE");
+            pelletTexture = Content.Load<Texture2D>(@"Square");
+            pelletSM = Content.Load<Texture2D>(@"PelletSnoopmode");
             gtexture = Content.Load<Texture2D>(@"Gdir");
             stexture = Content.Load<Texture2D>(@"Sdir");
+            spaceKey = Content.Load<Texture2D>(@"SpaceKey");
+            spaceDesc = Content.Load<Texture2D>(@"SpaceDesc");
+
+            int randi = rand.Next(0, 8);
+            switch (randi)
+            {
+                case 0:
+                    song = Content.Load<Song>(@"chiptune1");
+                    break;
+                case 1:
+                    song = Content.Load<Song>(@"chiptune2");
+                    break;
+                case 2:
+                    song = Content.Load<Song>(@"chiptune3");
+                    break;
+                case 3:
+                    song = Content.Load<Song>(@"chiptune4");
+                    break;
+                case 4:
+                    song = Content.Load<Song>(@"chiptune5");
+                    break;
+                case 5:
+                    song = Content.Load<Song>(@"chiptune6");
+                    break;
+                case 6:
+                    song = Content.Load<Song>(@"chiptune7");
+                    break;
+                case 7:
+                    song = Content.Load<Song>(@"chiptune8");
+                    break;
+            }
+            MediaPlayer.Play(song);
+            snoop = Content.Load<SoundEffect>(@"mode");
+            snoopi = snoop.CreateInstance();
+            snoopi.IsLooped = true;
+
+            snoopend = Content.Load<SoundEffect>(@"modeend");
+            snoopei = snoopend.CreateInstance();
+
+            lose = Content.Load<SoundEffect>(@"lose");
+            losei = lose.CreateInstance();
 
             // TODO: use this.Content to load your game content here
         }
@@ -107,50 +160,76 @@ namespace SnakesOnAGame
             // TODO: Add your update logic here
             KeyboardState ks = Keyboard.GetState();
 
-            if (snoopmode) speed = 0.5f;
-            else speed = 1;
+            int milli = (int)gameTime.ElapsedGameTime.TotalMilliseconds;
 
-            // Movement
-            if (ks.IsKeyDown(Keys.Up)) direction = new Vector2(0, -speed);
-            else if (ks.IsKeyDown(Keys.Down)) direction = new Vector2(0, speed);
-            else if (ks.IsKeyDown(Keys.Left)) direction = new Vector2(-speed, 0);
-            else if (ks.IsKeyDown(Keys.Right)) direction = new Vector2(speed, 0);
+            timeSinceLastUpdate += milli;
+            snoopTime += milli;
+            gameOverTime += milli;
 
-            // Snoop Mode
-            if (ks.IsKeyDown(Keys.S) && snoopmode == false) { snoopmode = true; Window.Title = title + " [Snoop Mode]"; }
-            else if (ks.IsKeyDown(Keys.S) && snoopmode) { snoopmode = false; Window.Title = title; }
-
-            if (snoopmode) { col = Color.Green; ballcol = Color.GreenYellow; }
-            else { col = Color.Red; ballcol = Color.Yellow; }
-
-            // No leaving the window
-            Rectangle bounds = this.Window.ClientBounds;
-
-            if (snake.Count > 0)
+            if (timeSinceLastUpdate >= millisecondsPerFrame)
             {
-                if (snake[0].Y > this.GraphicsDevice.Viewport.Bounds.Height / 2) gameover = true;
+                timeSinceLastUpdate = 0;
 
-                for (int i = snake.Count - 1; i > 0; i--)
+                if (snoopmode)
                 {
-                    snake[i] = snake[i - 1];
+                    millisecondsPerFrame = 60;
+                    MediaPlayer.Volume = 0.04f;
+                    snoopi.Play();
+                    if (snoopTime > 19615) { snoopTime = 0; snoopmode = false; snoopmodeFinished = true; } // Snoop Mode lasts ~20 seconds
+                    showdirections = false;
+                }
+                else
+                {
+                    millisecondsPerFrame = 30;
+                    MediaPlayer.Volume = 0.3f;
+                    snoopi.Stop();
                 }
 
-                snake[0] += direction;
+                // Movement
+                if (ks.IsKeyDown(Keys.Up)) direction = new Vector2(0, -1);
+                else if (ks.IsKeyDown(Keys.Down)) direction = new Vector2(0, 1);
+                else if (ks.IsKeyDown(Keys.Left)) direction = new Vector2(-1, 0);
+                else if (ks.IsKeyDown(Keys.Right)) direction = new Vector2(1, 0);
 
-                if (snake[0] == food)
+                // Snoop Mode
+                if (ks.IsKeyDown(Keys.Space) && snoopmode == false) { snoopmode = true; Window.Title = title + " [Snoop Mode]"; }
+                else if ((ks.IsKeyDown(Keys.Space) && snoopmode) || snoopmodeFinished) { snoopmode = false; Window.Title = title; snoopei.Play(); snoopmodeFinished = false; }
+
+                if (snoopmode) { col = Color.Green; ballcol = Color.GreenYellow; }
+                else { col = Color.Red; ballcol = Color.Yellow; }
+
+                // No leaving the window
+                Rectangle bounds = this.Window.ClientBounds;
+
+                if (snake.Count > 0)
                 {
-                    int irandx = rand.Next(bounds.Left, bounds.Right);
-                    int irandy = rand.Next(bounds.Top, bounds.Bottom);
-                    food = new Vector2(irandx, irandy);
-                    snake.Add(new Vector2(snake[0].X, snake[0].Y - snake.Count * 20));
+                    for (int i = snake.Count - 1; i > 0; i--)
+                    {
+                        snake[i] = snake[i - 1];
+                        if (snake[i].X > bounds.Height) gameover = true;
+                    }
+                    for (int i = snake.Count - 1; i > 1; i--)
+                        if (snake[0] == snake[i]) gameover = true;
+
+                    snake[0] += direction;
+
+                    if (snake[0] == food)
+                    {
+                        int irandx = rand.Next(16, bounds.Width - 16);
+                        int irandy = rand.Next(16, bounds.Height - 16);
+                        food = new Vector2(irandx, irandy);
+                        snake.Add(new Vector2(snake[0].X, snake[0].Y - snake.Count * 20));
+                    }
+                }
+
+                if (gameover)
+                {
+                    snake.Clear();
+                    MediaPlayer.Stop();
+                    if (gameOverSoundFinished == false) { losei.Play(); gameOverSoundFinished = true; }
+                    //if (gameOverTime > 3000) Exit();
                 }
             }
-
-            if (gameover)
-            {
-                snake.Clear();
-            }
-
             base.Update(gameTime);
         }
 
@@ -164,22 +243,28 @@ namespace SnakesOnAGame
 
             spriteBatch.Begin();
             // TODO: Add your drawing code here
-            spriteBatch.Draw(background, new Vector2(0, 0), Color.White);
+            if (snoopmode) spriteBatch.Draw(backgroundSM, new Vector2(0, 0), Color.LightGreen);
+            else spriteBatch.Draw(background, new Vector2(0, 0), Color.White);
 
-                            Rectangle bounds = this.Window.ClientBounds;
+            Rectangle bounds = this.Window.ClientBounds;
 
             for (int i = 0; i < snake.Count; i++)
                 spriteBatch.Draw(snakeTexture, new Rectangle((int)snake[i].X * 20, (int)snake[i].Y * 20, 20, 20), new Rectangle(0, 0, snakeTexture.Width, snakeTexture.Height), col);
 
+            if (snoopmode) spriteBatch.Draw(pelletSM, food, Color.White);
+            else spriteBatch.Draw(pelletTexture, food, ballcol);
+
             if (gameover)
             {
-                Vector2 pos = new Vector2(bounds.Width/2, bounds.Height/2) - new Vector2(overTexture.Width/2, overTexture.Height/2);
+                Vector2 pos = new Vector2(bounds.Width / 2 - overTexture.Width / 2, bounds.Height / 2 - overTexture.Height / 2);
                 spriteBatch.Draw(overTexture, pos, Color.White);
             }
 
-            spriteBatch.Draw(pelletTexture, food, ballcol);
-            spriteBatch.Draw(gtexture, new Vector2(bounds.Center.X - gtexture.Width, bounds.Center.Y - gtexture.Height), Color.White);
-            spriteBatch.Draw(stexture, new Vector2(bounds.Center.X - stexture.Width, bounds.Center.Y - stexture.Height + 100), Color.White);
+            if (showdirections)
+            {
+                spriteBatch.Draw(spaceKey, new Vector2(bounds.Width - spaceKey.Width - 20, bounds.Height - spaceKey.Height - spaceDesc.Height - 20), Color.White);
+                spriteBatch.Draw(spaceDesc, new Vector2(bounds.Width - spaceDesc.Width - 20, bounds.Height - spaceDesc.Height - 20), Color.White);
+            }
 
             spriteBatch.End();
 
